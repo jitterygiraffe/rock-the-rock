ac063a4466e08194a6992dcc2003d165a32f4a8e
okwiridesigns <okwirioduor@gmail.com>
2025-01-24 21:31:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+//randomly returning one of the following string values: "rock", 
+//"paper", "scissors"
+function getComputerChoice(){
+    if (Math.floor(Math.random()*3) === 0) {
+        return console.log("rock") }
+     else if (Math.floor(Math.random() * 3) ===1) {
+        return console.log("paper")
+    } else if (Math.floor(Math.random() * 3) ===2) {
+        return console.log("scissors")
+    }
+};
+
+getComputerChoice();
